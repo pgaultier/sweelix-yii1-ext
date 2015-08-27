aeab447ade0cded66333ded02107edd90c35ffad
David Ghyse <dghyse@ibitux.com>
2015-08-27 11:23:54 +0200

    fix wrong function call for this feature

--- src/entities/Tag.php
+++ src/entities/Tag.php
@@ -165,7 +165,7 @@ class Tag extends ActiveRecordTag
      */
     public function getRoute($action = null)
     {
-        $route = RouteEncoder::encoder(null, null, $this->tagId) . '/';
+        $route = RouteEncoder::encode(null, null, $this->tagId) . '/';
         if (empty($action) === false) {
             $route = $route . $action;
         }
